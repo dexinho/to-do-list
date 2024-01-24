22b9e5e232b3fefbdb93bd760e724c965e5326ca
Jasmin Hidanovic <dexinho008@gmail.com>
2024-01-24 23:47:30 +0100

    added handleMenuOptionSelect

--- src/App.js
+++ src/App.js
@@ -1,22 +1,52 @@
 import React, { useState } from "react";
-import { AddTask } from "./components/AddTask";
+import { AddTask } from "./components/AddTask/AddTask";
+import { TaskMenu } from "./components/TaskMenu/TaskMenu";
 
 export const App = () => {
   const [tasks, setTasks] = useState([]);
+  const [selectedTask, setSelectedTask] = useState(null);
+  const [menuPosition, setMenuPosition] = useState({ x: 0, y: 0 });
 
-  const handleTaskAddition = (task) => {
-    setTasks([...tasks, task]);
+  const handleAddTask = (newTask) => {
+    setTasks((prevTasks) => [...prevTasks, newTask]);
+  };
+
+  const handleTaskClick = (index, event) => {
+    console.log(`Task clicked: ${index}`);
+    const x = event.clientX;
+    const y = event.clientY;
+    setSelectedTask(index);
+    setMenuPosition({ x, y });
+  };
+
+  const handleMenuOptionSelect = (option) => {
+    console.log(`Option selected for task ${selectedTask + 1}: ${option}`);
+    // Add your logic for handling menu option selection
+    setSelectedTask(null); // Reset selected task
   };
 
   return (
     <div>
-      <AddTask onTaskAddition={handleTaskAddition} />
-      <div>Tasks: </div>
-      <ol>
+      <h1>Task List App</h1>
+      <AddTask onTaskAddition={handleAddTask} />
+      <ul>
         {tasks.map((task, index) => (
-          <li key={index}>{task}</li>
+          <li key={index} onClick={(e) => handleTaskClick(index, e)}>
+            {task}
+            {selectedTask === index && (
+              <TaskMenu
+                onSelectOption={handleMenuOptionSelect}
+                style={{
+                  left: `${menuPosition.x}px`,
+                  top: `${menuPosition.y}px`,
+                }}
+              />
+            )}
+          </li>
         ))}
-      </ol>
+      </ul>
     </div>
   );
 };
+
+export default App;
