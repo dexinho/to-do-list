4206ba8df733259cd72093297626d70ec6dcb3b4
Jasmin Hidanovic <dexinho008@gmail.com>
2024-01-26 14:43:56 +0100

    added put and delete

--- backend/routers/tasksRouter.js
+++ backend/routers/tasksRouter.js
@@ -4,8 +4,8 @@ const tasksController = require("../controllers/tasksController");
 const router = express.Router();
 
 router.get("/tasks", tasksController.getTasks);
-router.post("/tasks", tasksController.postTasks);
-// router.delete("/tasks:id", tasksController.deleteTask);
-// router.put("/tasksLid", tasksController.updateTask);
+router.post("/tasks", tasksController.postTask);
+router.delete("/tasks:id", tasksController.deleteTask);
+router.put("/tasks:id", tasksController.putTask);
 
 module.exports = router;
