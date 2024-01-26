56deec5d25432c23365da0206b21ae2c80a5cfb8
Jasmin Hidanovic <dexinho008@gmail.com>
2024-01-26 01:12:42 +0100

    added useEffect that handles task menu clicks

--- src/components/TaskManager/TaskMenu.js
+++ src/components/TaskManager/TaskMenu.js
@@ -1,19 +1,29 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import "./TaskMenu.css";
 
-export const TaskMenu = ({ isVisible, onClose, taskOffsets }) => {
+export const TaskMenu = ({
+  isVisible,
+  onTaskMenuClose,
+  taskOffsets,
+  taskMenuOptions,
+  isTaskCompleted,
+}) => {
   const taskMenuRef = useRef(null);
+  const taskMenuRenameInputRef = useRef(null);
+  const [taskRename, setTaskRename] = useState("");
 
   useEffect(() => {
+    taskMenuRenameInputRef.current.focus();
     const handleClickOutside = (e) => {
       if (taskMenuRef.current && !taskMenuRef.current.contains(e.target)) {
-        onClose();
+        onTaskMenuClose();
       }
     };
 
     if (isVisible) {
+      const menuHeight = parseInt(getComputedStyle(taskMenuRef.current).height, 10)
       taskMenuRef.current.style.left = `${taskOffsets.x}px`;
-      taskMenuRef.current.style.top = `${taskOffsets.y}px`;
+      taskMenuRef.current.style.top = `${taskOffsets.y - menuHeight}px`;
       document.addEventListener("mousedown", handleClickOutside);
     } else {
       document.removeEventListener("mousedown", handleClickOutside);
@@ -24,13 +34,63 @@ export const TaskMenu = ({ isVisible, onClose, taskOffsets }) => {
     };
   }, [isVisible]);
 
+  const handleTaskMenuOption = (option) => {
+    if (option === "Delete") taskMenuOptions.deleteTask();
+    else if (option === "Rename") taskMenuOptions.renameTask(taskRename);
+    else if (option === "Complete") taskMenuOptions.toCompleteTask(true);
+    else if (option === "Incomplete")
+      taskMenuOptions.toCompleteTask(false);
+
+    taskMenuRenameInputRef.current.value = "";
+    setTaskRename("");
+
+    onTaskMenuClose();
+  };
+
+  const handleTaskRename = (e) => {
+    setTaskRename(e.target.value);
+  };
+
+  const isCompleted = isTaskCompleted ? "Incomplete" : "Complete";
+
   return (
     <div
       className={`task-menu ${isVisible ? "visible" : ""}`}
       ref={taskMenuRef}
     >
-      <div>Edit</div>
-      <div>Delete</div>
+      <div>Task Menu</div>
+      <div>
+        <div className="task-rename-holder">
+          <div>
+            <input
+              id="task-rename-input"
+              placeholder="new task name"
+              ref={taskMenuRenameInputRef}
+              onChange={handleTaskRename}
+              type="text"
+            ></input>
+          </div>
+          <div
+            className="task-menu-option rename-task"
+            onClick={() => handleTaskMenuOption("Rename")}
+          >
+            Rename
+          </div>
+        </div>
+      </div>
+      <div
+        className="task-menu-option complete-task"
+        onClick={() => handleTaskMenuOption(isCompleted)}
+      >
+        {isCompleted}
+      </div>
+
+      <div
+        className="task-menu-option delete-task"
+        onClick={() => handleTaskMenuOption("Delete")}
+      >
+        Delete
+      </div>
     </div>
   );
 };
