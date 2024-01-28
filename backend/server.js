05a26b58dd88392057db7f9fa05684e4ff02a11e
Jasmin Hidanovic <dexinho008@gmail.com>
2024-01-28 23:58:16 +0100

    added delete all from db

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const connectDB = require("./models/mongoConnection");
 const app = express();
 
 const tasksRouter = require("./routers/tasksRouter");
+// const Tasks = require("./models/Tasks");
 
 app.use(express.json());
 app.use(cors());
@@ -12,6 +13,18 @@ app.use(cors());
 connectDB();
 
 app.use("/api", tasksRouter);
+// app.get("/delete", async (req, res) => {
+//   try {
+//     const result = await Tasks.deleteMany();
+//     res.json({
+//       message: "All tasks deleted successfully",
+//       deletedCount: result.deletedCount,
+//     });
+//   } catch (error) {
+//     console.error("Error deleting tasks:", error);
+//     res.status(500).json({ message: "Server error" });
+//   }
+// });
 
 const PORT = 5000;
 app.listen(PORT, () => {
