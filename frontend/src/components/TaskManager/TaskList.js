425f6924ae25f2a2d3b46af6c70734dd417045f2
Jasmin Hidanovic <dexinho008@gmail.com>
2024-01-28 23:59:12 +0100

    added put and delete

--- frontend/src/components/TaskManager/TaskList.js
+++ frontend/src/components/TaskManager/TaskList.js
@@ -2,13 +2,14 @@ import React from "react";
 import "./TaskList.css";
 
 export const TaskList = ({ handleTaskClick, tasks }) => {
+  console.log(tasks);
   return (
     <ol className="task-list">
-      {tasks.map((task, index) => (
+      {tasks.map((task) => (
         <li
           className={`task-element ${task.isCompleted ? "completed" : ""}`}
-          onClick={(e) => handleTaskClick({ e, index })}
-          key={index}
+          onClick={(e) => handleTaskClick({ e, _id: task._id })}
+          key={task._id}
         >
           {task.name}
         </li>
