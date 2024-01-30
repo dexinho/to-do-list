50740e1b30b4ba9552bb685433a0b5fb47abb55d
Jasmin Hidanovic <dexinho008@gmail.com>
2024-01-30 21:22:56 +0100

    changed tasks div

--- frontend/src/components/TaskManager/TaskList.js
+++ frontend/src/components/TaskManager/TaskList.js
@@ -2,18 +2,20 @@ import React from "react";
 import "./TaskList.css";
 
 export const TaskList = ({ handleTaskClick, tasks }) => {
-  console.log(tasks);
   return (
-    <ol className="task-list">
-      {tasks.map((task) => (
-        <li
-          className={`task-element ${task.isCompleted ? "completed" : ""}`}
-          onClick={(e) => handleTaskClick({ e, _id: task._id })}
-          key={task._id}
-        >
-          {task.name}
-        </li>
-      ))}
-    </ol>
+    <div>
+      <div>{tasks.length > 0 ? "Tasks" : "No tasks"}: </div>
+      <ol className="task-list">
+        {tasks.map((task) => (
+          <li
+            className={`task-element ${task.isCompleted ? "completed" : ""}`}
+            onClick={(e) => handleTaskClick({ e, _id: task._id })}
+            key={task._id}
+          >
+            {task.name}
+          </li>
+        ))}
+      </ol>
+    </div>
   );
 };
