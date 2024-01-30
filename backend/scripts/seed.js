88c5dbc14ea07bc98738189e408f8f449b2a482f
Jasmin Hidanovic <dexinho008@gmail.com>
2024-01-30 21:24:31 +0100

    deleted old files

--- backend/scripts/seed.js
+++
@@ -1,9 +0,0 @@
-// const mongoConnection = require("../models/mongoConnection");
-// const seed = require("./seedData");
-
-// const mongoURI = "mongodb+srv://root:root@cluster0.mltyjxq.mongodb.net/";
-// mongoConnection(mongoURI)
-//   .then(() => seed())
-//   .catch((error) => {
-//     console.error("Seed script error:", error);
-//   });
